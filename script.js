de3b3343d1cdbbeb082c6b4a5c6bbd4ea762dbed
geometry-dash-world <benhanry542+web@gmail.com>
2025-01-31 16:07:52 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
-function openFullscreen() {
-    let game = document.querySelector(".game-iframe");
-    if (game.requestFullscreen) {
-        game.requestFullscreen();
-    } else if (game.mozRequestFullScreen) { /* Firefox */
-        game.mozRequestFullScreen();
-    } else if (game.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
-        game.webkitRequestFullscreen();
-    } else if (game.msRequestFullscreen) { /* IE/Edge */
-        game.msRequestFullscreen();
+// Google Analytics Tag
+(function loadGoogleAnalytics() {
+  const script = document.createElement("script");
+  script.async = true;
+  script.src = "https://www.googletagmanager.com/gtag/js?id=G-37JB6K02HZ";
+  document.head.appendChild(script);
+
+  script.onload = function () {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      dataLayer.push(arguments);
     }
+    gtag('js', new Date());
+    gtag('config', 'G-37JB6K02HZ');
+  };
+})();
+
+// Fullscreen Functionality
+function openFullscreen() {
+  let game = document.querySelector(".game-iframe");
+  if (game.requestFullscreen) {
+    game.requestFullscreen();
+  } else if (game.mozRequestFullScreen) { /* Firefox */
+    game.mozRequestFullScreen();
+  } else if (game.webkitRequestFullscreen) { /* Chrome, Safari, and Opera */
+    game.webkitRequestFullscreen();
+  } else if (game.msRequestFullscreen) { /* IE/Edge */
+    game.msRequestFullscreen();
+  }
 }
